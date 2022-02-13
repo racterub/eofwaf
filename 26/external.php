ccd7f81581a2b3d0868c3bdcabc4aabc31abe55c
racterub <racterub@gmail.com>
2022-02-13 11:15:58 +0800

    update waf

--- 26/external.php
+++ 26/external.php
@@ -1,31 +1,8 @@
-<?php // Your WAF goes here
-
-$d=['S'=>$_SERVER,'R'=>$_REQUEST];
-$l = json_encode($d);
-
-$h=curl_init('http://10.14.15.119:8887/logger.php');
-curl_setopt($h,CURLOPT_POST,1);
-curl_setopt($h,CURLOPT_POSTFIELDS,$l);
-// curl_setopt($h, CURLOPT_CONNECTTIMEOUT, 1);
-// curl_setopt($h, CURLOPT_TIMEOUT, 1);
-@curl_exec($h);
-@curl_close($h);
-if(isset($_POST['url']) && (substr($_POST['url'],0,strlen("http"))!=="http")) die();
+<?php
+if(isset($_POST['url'])){
+ $u=$_POST['url'];if(!strpos($u,'http')!==0)die();
+}
 foreach ($_REQUEST as $k => $v) {
-$_REQUEST[$k]=preg_replace("/kill/",'',$v);
+$_REQUEST[$k]=preg_replace("/['\\\\]/",'',$v);
+$_REQUEST[$k]=preg_replace("/flag/",'',$v);
 }
-function c($b){
-    if(strlen($b) == 0)return $b;
-    $h=curl_init('http://10.14.15.119:8887/logger.php');
-    curl_setopt($h,CURLOPT_POST,1);
-    curl_setopt($h,CURLOPT_POSTFIELDS,json_encode(array("resp"=>$b,"time"=>time())));
-    // curl_setopt($h, CURLOPT_CONNECTTIMEOUT, 1);
-    // curl_setopt($h, CURLOPT_TIMEOUT, 1);
-    @curl_exec($h);
-    @curl_close($h);
-    $p = array(str_rot13('/RBS').'\{.{5}/');
-    $r = array(str_rot13("RBS{").substr(md5(microtime()),rand(0,26),5));
-    return preg_replace($p, $r, $b);
-}
-
-ob_start(chr(99));

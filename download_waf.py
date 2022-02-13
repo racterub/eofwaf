425665259f96b31c22944f34accb93235bf6b4fe
racterub <racterub@gmail.com>
2022-02-13 09:43:01 +0800

    update waf

--- download_waf.py
+++ download_waf.py
@@ -9,10 +9,8 @@ scan_range = [37]
 for i in scan_range:
     r = requests.get(f"http://chals2.eof.ais3.org:8000/waf/{i}")
     external = re.findall('@include(?:_once)?\s"\/var\/www\/html\/(.+)";', r.text)
-    print(external)
     if external:
         external_waf = requests.get(f"http://chals2.eof.ais3.org:{30000+i}/{external[0]}")
-    print(f"http://chals2.eof.ais3.org:{30000+i}/{external[0]}")
     if external_waf.text:
         with open(f"{i}/external.php", "w") as f:
             f.write(external_waf.text)
